610e5fdf512608ae6611d77c786aea3037a6f1d4
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-12-28 08:33:19 +0900

    update  table

--- R/tables.R
+++ R/tables.R
@@ -13,14 +13,18 @@ segments_sep <- map_dfr(tmp$fit_segments,
   \(x)x$summary |> filter(variable %in% c("log_a", "b", "a")))  |>
   mutate(species = rep(tmp$species, each = 3))
 
-segments_sep |>
-  filter(variable == "a") |>
-  mutate_if(is.numeric, \(x) round(x, digits = 2)) |>
-  mutate_if(is.numeric, \(x) format(x, nsmall = 2, trim = TRUE)) |>
-  mutate(multilevel_sep_a = str_c(q50, " [", q2.5, ", ", q97.5, "]")) |>
-  dplyr::select(-variable:-ess_tail)
+pool_sep <- map_dfr(tmp$fit_pool,
+  \(x)x$summary |> filter(variable %in% c("log_a", "b", "a")))  |>
+  mutate(species = rep(tmp$species, each = 3))
 
-clean_sep <- function(data, var, name) {
+names_data <- xylem_lab |>
+  mutate(alpha1 = str_replace_all(sp_num1, "_", ",")) |>
+  mutate(alpha1 = str_c("alpha[", alpha1, "]")) |>
+  mutate(alpha2 = str_replace_all(alpha1, "1,", "2,")) |>
+  mutate(a = str_c("a[", sp_num, "]")) |>
+  pivot_longer(alpha1:a, values_to = "variable")
+
+clean_sep <- function(data, names_data, var, name) {
   data <- data |>
     filter(variable == var) |>
     mutate_if(is.numeric, \(x) round(x, digits = 2)) |>
@@ -31,10 +35,10 @@ clean_sep <- function(data, var, name) {
   data
 }
 
-clean_all <- function(data, var, name) {
+clean_all <- function(data, names_data, var, name) {
    data <- data |>
     filter(str_detect(variable, "^a")) |>
-    full_join(names_data) |>
+    full_join(names_data, by = "variable") |>
     dplyr::select(species, xylem_long_fct, variable, q50, q2.5, q97.5) |>
     mutate_if(is.numeric, \(x) round(x, digits = 2)) |>
     mutate_if(is.numeric, \(x) format(x, nsmall = 2, trim = TRUE)) |>
@@ -56,19 +60,18 @@ clean_all <- function(data, var, name) {
     dplyr::select(-variable:-q97.5)
 }
 
-
-clean_all(segments, "a", "multi_full_a") |>
-  full_join(clean_all(segments, "ln_a", "multi_full_ln_a")) |>
-  full_join(clean_all(segments, "b", "multi_full_b")) |>
-  full_join(clean_all(pool, "a", "pool_full_a")) |>
-  full_join(clean_all(pool, "ln_a", "pool_full_ln_a")) |>
-  full_join(clean_all(pool, "b", "pool_full_b")) |>
-  full_join(clean_sep(segments_sep, "a", "multi_sep_a")) |>
-  full_join(clean_sep(segments_sep, "log_a", "multi_sep_ln_a")) |>
-  full_join(clean_sep(segments_sep, "b", "multi_sep_a")) |>
-  full_join(clean_sep(pool_sep, "a", "pool_sep_a")) |>
-  full_join(clean_sep(pool_sep, "log_a", "pool_sep_ln_a")) |>
-  full_join(clean_sep(pool_sep, "b", "pool_sep_b")) |>
+clean_all(segments, names_data, "a", "multi_full_a") |>
+  full_join(clean_all(segments, names_data, "ln_a", "multi_full_ln_a")) |>
+  full_join(clean_all(segments, names_data, "b", "multi_full_b")) |>
+  full_join(clean_all(pool, names_data, "a", "pool_full_a")) |>
+  full_join(clean_all(pool, names_data, "ln_a", "pool_full_ln_a")) |>
+  full_join(clean_all(pool, names_data, "b", "pool_full_b")) |>
+  full_join(clean_sep(segments_sep, names_data, "a", "multi_sep_a")) |>
+  full_join(clean_sep(segments_sep, names_data, "log_a", "multi_sep_ln_a")) |>
+  full_join(clean_sep(segments_sep, names_data, "b", "multi_sep_a")) |>
+  full_join(clean_sep(pool_sep, names_data, "a", "pool_sep_a")) |>
+  full_join(clean_sep(pool_sep, names_data, "log_a", "pool_sep_ln_a")) |>
+  full_join(clean_sep(pool_sep, names_data, "b", "pool_sep_b")) |>
   DT::datatable()
 
 ab_data <- full_join(segments_a, segments_b) |>
