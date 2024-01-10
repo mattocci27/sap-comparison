c22ed2fa21b2861c7be9337eb12c6f35c4b1db99
Masatoshi Katabuchi <mattocci27@gmail.com>
2024-01-11 01:01:53 +0900

    update scale fig colors

--- R/scale.R
+++ R/scale.R
@@ -63,7 +63,7 @@ ab_example <- function(full_df_processed, post1, post2, summary_stats_list, ab_s
 
   p1 <- ggplot(full_df_processed, aes(x = exp(log_k))) +
     geom_histogram() +
-    xlab(expression("K "((Delta~T[max]-Delta~T)/Delta~T))) +
+    xlab("K") +
     theme_bw()
 
   log_k <- seq(0.001, 0.5, length = 100) |> log()
@@ -77,9 +77,15 @@ ab_example <- function(full_df_processed, post1, post2, summary_stats_list, ab_s
     mutate(pg = ifelse(name == "y1", "0.02", "0.08")) |>
     ggplot(aes(x = exp(log_k), y = value, col = pg)) +
     geom_line() +
-    xlab(expression("K "((Delta~T[max]-Delta~T)/Delta~T))) +
+    xlab("K") +
     ylab(expression("Sap flux density "(g~m^{-2}~s^{-1}))) +
-    theme_bw()
+    labs(color = expression(italic(P[g])~(MPa~m^{-1}))) +
+    theme_bw() +
+    theme(
+      legend.position = "right",
+      legend.key.width = unit(0.2, "cm"),
+      legend.key.height = unit(0.4, "cm")
+    )
 
   max_lab <- summary_stats_list$max_lab
   p3 <- summary_stats_list$df |>
@@ -88,16 +94,17 @@ ab_example <- function(full_df_processed, post1, post2, summary_stats_list, ab_s
     ggplot(aes(x = fd, y = fd2)) +
     # geom_line(aes( group = tree_dir)) +
     # geom_point(alpha = 0.2) +
-    geom_point(alpha = 0.3, aes(color = k)) +
-    scale_color_viridis_c(name = "K") +
+    geom_point(alpha = 0.2, aes(color = k)) +
+    scale_color_viridis_c(name = "K", option = "C") +
     geom_abline(intercept = 0, slope = 1, lty = 2) +
     ylab(expression(Sap~flux~(g~s^{-1}) * ";"~italic(P[g])==0.08~(MPa ~m^{-1}))) +
     xlab(expression(Sap~flux~(g~s^{-1}) * ";"~italic(P[g])==0.02~(MPa ~m^{-1}))) +
     coord_cartesian(xlim = c(0, max_lab), ylim = c(0, max_lab)) +
     theme_bw() +
     theme(
-      legend.position = c(0.8, 0.2),
-      legend.key.width = unit(0.5, "cm")
+      legend.position = c(0.85, 0.38),
+      legend.key.width = unit(0.2, "cm"),
+      legend.key.height = unit(0.4, "cm")
     )
 
   segments_post2 <- segments_post %>%
@@ -105,18 +112,26 @@ ab_example <- function(full_df_processed, post1, post2, summary_stats_list, ab_s
     mutate(id = as.character(id))
   tmp <- full_join(ab_summarized_df, segments_post2)
   p4 <- ggplot(tmp |> sample_n(1000), aes(x = exp(log_a), y = b, col = tr)) +
-    geom_point(alpha = 0.8) +
-    scale_color_viridis_c(name = expression("Transpiration (mm"~y^-1*")"), option = "C") +
+    geom_point(alpha = 0.9) +
+    scale_color_viridis_c(name = expression("Transpiration (mm"~y^-1*")"), option = "D") +
     scale_x_log10() +
     xlab(expression("Coefficient"~italic(a))) +
     ylab(expression("Coefficient"~italic(b))) +
-    # my_theme() +
     theme_bw() +
     theme(
-      legend.position = "right"
+      legend.position = "right",
+      legend.key.width = unit(0.2, "cm"),
+      legend.key.height = unit(0.4, "cm")
     )
 
-  p1 + p2 + p3 + p4 + plot_layout(ncol = 2, nrow = 2)
+  p1 + p2 + p3 + p4 + plot_layout(ncol = 2, nrow = 2) +
+     plot_annotation(tag_levels = "A") &
+     theme(
+      axis.title = element_text(size = 8),
+      axis.text = element_text(size = 7),
+      legend.title = element_text(size = 8),
+      legend.text = element_text(size = 7)
+     )
   # my_ggsave(
   #   here("figs/test"),
   #   p,
